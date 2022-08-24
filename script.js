8ecc5daee75f86e6d6566c45755cefdef4831cc0
erikgalindo40 <erikgalindo40@yahoo.com>
2022-08-24 00:35:16 -0700

    add html file, add css file, and add js file

---
+++ script.js
@@ -0,0 +1,88 @@
+const body = document.getElementById('body')
+const grid = document.getElementById('grid')
+const gridSizeDefiner = document.getElementById('gridSizeDefiner')
+const submitButton = document.getElementById('submitButton')
+const initButton = document.getElementById('initButton')
+const yesAnswerButton = document.getElementById('yesAnswerButton')
+const noAnswerButton = document.getElementById('noAnswerButton')
+const questionContainer = document.getElementById('questionContainer')
+const clearGridButton = document.getElementById('clearGridButton')
+let currentColor = '#18AFA5'
+let isDrawing = false
+let isDrawnOn = false
+let isGrid = false
+
+function startDrawing() { //stored in installEventListeners
+    if(this.dataset.nocolor == 'nocolor') { 
+        isDrawing=true
+    } else {
+        this.style.background = currentColor
+        isDrawing = true}
+}
+
+function continueDrawing() { //stored in installEventListeners
+    if (isDrawing) {
+        this.style.background = currentColor
+    }
+}
+
+function stopDrawing() { //stored in installEventListeners
+    isDrawing = false
+}
+
+function installGridEventListeners() { //stored in installEventListeners
+    grid.addEventListener('click', ()=>{isDrawnOn=true})
+    grid.addEventListener('mousedown', startDrawing)
+    grid.addEventListener('mouseleave', stopDrawing)
+    grid.addEventListener('mouseup', stopDrawing)
+}
+
+function installEventListeners() { //stored in createGridSize
+    let listItems = document.getElementsByTagName('li')
+    for (let item of listItems) {
+        item.addEventListener('mousedown', startDrawing)
+        item.addEventListener('mouseover', continueDrawing)
+        item.addEventListener('mouseup', stopDrawing)
+    }
+    installGridEventListeners()
+}
+
+function toggleQuestionDisplay() {
+    body.classList.toggle('hide')
+    questionContainer.classList.toggle('hide')
+
+}
+
+function createGridSize() {
+    if (isDrawnOn) {
+        toggleQuestionDisplay()
+    } else {
+    let gridSizeUserInput = gridSizeDefiner.value
+    if (gridSizeUserInput > 0 && gridSizeUserInput <= 75) {
+        if (grid.innerHTML != '') {grid.innerText=''} //clears current grid
+        let gridUnit = document.createElement('li')
+        let i=0
+
+        grid.style.gridTemplateColumns = `repeat(${gridSizeUserInput},auto)`
+        while (i < Math.pow(gridSizeUserInput, 2)) {
+             grid.appendChild(gridUnit.cloneNode())
+            i++
+        }
+        installEventListeners()
+        isDrawnOn=false
+    }}
+    isGrid=true
+}
+
+clearGridButton.addEventListener('click', ()=>{
+    if (isGrid && isDrawnOn) {
+        toggleQuestionDisplay()
+    }
+})
+yesAnswerButton.addEventListener('click',()=>{
+        isDrawnOn=false
+        createGridSize()
+        toggleQuestionDisplay()
+})
+noAnswerButton.addEventListener('click', toggleQuestionDisplay)
+submitButton.addEventListener('click', createGridSize)
